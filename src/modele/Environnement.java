30d5d016271e05e552ff939034ab5ce7531b8f94
Haris <amaroucheharis@gmail.com>
2020-05-11 15:32:37 +0200

    version absolument pas poussée de l'environnement

---
+++ src/modele/Environnement.java
@@ -0,0 +1,24 @@
+package modele;
+
+import java.util.ArrayList;
+
+public class Environnement {
+	
+	private ArrayList<Zombie> listeZombie;
+	private ArrayList<Tourelle> listeTourelle;
+	
+	public Environnement() {
+		listeZombie = new ArrayList<Zombie>();
+		listeTourelle = new ArrayList<Tourelle>();
+	}
+	
+	public void unTour() {
+		for(Zombie currentZombie : this.listeZombie) {
+			currentZombie.agit();
+		}
+		
+		for(Tourelle currentTourelle : this.listeTourelle) {
+			currentTourelle.agit();
+		}
+	}
+}
