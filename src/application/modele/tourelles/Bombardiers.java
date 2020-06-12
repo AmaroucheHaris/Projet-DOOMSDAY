dd84c72592757d989123abace78b148ce840ced1
KenzoNs <knunes@iut.univ-paris8.fr>
2020-06-12 10:35:42 +0200

    grenadier v2

--- src/application/modele/tourelles/Bombardiers.java
+++ src/application/modele/tourelles/Bombardiers.java
@@ -20,8 +20,8 @@ public abstract class Bombardiers extends Tourelle {
 		for (Zombie zombie : zombies) {
 			int posXZombie =  zombie.getXProperty().getValue();
 			int posYZombie =  zombie.getYProperty().getValue();
-			if(zombie.estEnVie() && this.detecterAxeX(posXZombie) && this.detecterAxeY(posYZombie)) {	
-				System.out.println("Zombie détécté");
+			if(zombie.estEnVie() && this.detecterAxes(posXZombie, posYZombie)) {	
+				System.out.println("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
 				return zombie;
 			}
 		}
@@ -39,8 +39,6 @@ public abstract class Bombardiers extends Tourelle {
 			int posYFirstTarget = z.getYProperty().getValue();
 			for (Zombie zombie : zombies) {
 				if (zombie != z) {
-					System.out.println("Zombie 1:" + z);
-					System.out.println("Zombie 2:" + zombie);
 					int posXSecondTarget = zombie.getXProperty().getValue();
 					int posYSecondTarget = zombie.getYProperty().getValue();				
 					if(zombie.estEnVie() && posXSecondTarget - posXFirstTarget >= -porteeDegatZone && posXSecondTarget - posXFirstTarget <= porteeDegatZone && posYSecondTarget - posYFirstTarget >= -porteeDegatZone && posYSecondTarget - posYFirstTarget <= porteeDegatZone) {	
@@ -73,22 +71,25 @@ public abstract class Bombardiers extends Tourelle {
 	
 	public abstract void attaquer(Zombie target);
 
-	public boolean detecterAxeX(int posXZombie) {
-		System.out.println("résultat =  " + (posXZombie - this.getX()));
-		System.out.println("cas gauche max: " + (posXZombie - this.getX() >= -this.getPortee()));
-		System.out.println("cas gauche sécurité: " + (posXZombie - this.getX() <= -porteeLimite));
-		System.out.println("cas droit max: " + (posXZombie - this.getX() <= this.getPortee()));
-		System.out.println("cas droit sécurité: " + (posXZombie - this.getX() >= porteeLimite));
-		return posXZombie - this.getX() >= -this.getPortee() && posXZombie - this.getX() <= -porteeLimite && posXZombie - this.getX() <= this.getPortee() && posXZombie - this.getX() >= porteeLimite;
-	}
-	
-	public boolean detecterAxeY(int posYZombie) {
-		System.out.println("résultat =  " + (posYZombie - this.getY()));
-		System.out.println("cas haut max: " + (posYZombie - this.getY() >= -this.getPortee()));
-		System.out.println("cas haut sécurité: " + (posYZombie - this.getY() <= -porteeLimite));
-		System.out.println("cas bas max: " + (posYZombie - this.getY() <= this.getPortee()));
-		System.out.println("cas bas sécurité: " + (posYZombie - this.getY() >= porteeLimite));
-		return posYZombie - this.getY() >= -this.getPortee() && posYZombie - this.getY() <= -porteeLimite && posYZombie - this.getY() <= this.getPortee() && posYZombie - this.getY() >= porteeLimite;
+	public boolean detecterAxes(int posXZombie, int posYZombie) {
+		int resultatX = posXZombie - this.getX();
+		int resultatY = posYZombie - this.getY();
+		System.out.println("posX Tourelle: "+ this.getX());
+		System.out.println("posX Zombie: "+ posXZombie);
+		System.out.println("posY Zombie: "+ posYZombie);
+		System.out.println("posY Tourelle: "+ this.getY());
+		System.out.println("X resultat:" + resultatX);
+		System.out.println("Y resultat:" + resultatY);
+		if (resultatX >= 0 && resultatY >= 0) {
+//			System.out.println("cas droit max: " + (posXZombie - this.getX() <= this.getPortee()));
+//			System.out.println("cas droit sécurité: " + (posXZombie - this.getX() >= porteeLimite));
+			return posXZombie - this.getX() <= this.getPortee() && posXZombie - this.getX() >= porteeLimite && posYZombie - this.getY() <= this.getPortee() && posYZombie - this.getY() >= porteeLimite;
+		}
+		else {
+//			System.out.println("cas gauche max: " + (posXZombie - this.getX() >= -this.getPortee()));
+//			System.out.println("cas gauche sécurité: " + (posXZombie - this.getX() <= -porteeLimite));
+			return posXZombie - this.getX() >= -this.getPortee() && posXZombie - this.getX() <= -porteeLimite && posYZombie - this.getY() >= -this.getPortee() && posYZombie - this.getY() <= -porteeLimite;
+		}
 	}
 	
 	
